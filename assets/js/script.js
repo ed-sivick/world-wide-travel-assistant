e3419a16cf56a62516646e1b837b13e1b3078933
Nigara Sawirdin <nigaras@Nigaras-MBP.fios-router.home>
2020-09-28 19:46:20 -0400

    fixed Conflict

--- assets/js/script.js
+++ assets/js/script.js
@@ -76,7 +76,7 @@ $("#submit").click(function (event) {
                 startDay,
                 endDay
             );
-            // weatherforcastapi(lat, lon);
+            weatherforcastapi(city, citySearchList);
             coutryflag(countrycode);
         });
 });
@@ -200,60 +200,161 @@ function calendricapi(
         });
     }
 }
+
+// function createCityList(citySearchList) {
+//     $("#city-list").empty();
+  
+//     var keys = Object.keys(citySearchList);
+//     for (var i = 0; i < keys.length; i++) {
+//       var cityListEntry = $("<button>");
+//       cityListEntry.addClass("list-group-item list-group-item-action");
+  
+//       var splitStr = keys[i].toLowerCase().split(" ");
+//       for (var j = 0; j < splitStr.length; j++) {
+//         splitStr[j] =
+//           splitStr[j].charAt(0).toUpperCase() + splitStr[j].substring(1);
+//       }
+//       var titleCasedCity = splitStr.join(" ");
+//       cityListEntry.text(titleCasedCity);
+  
+//       $("#city-list").append(cityListEntry);
+//     }
+//   }
+
 // End Calendricapi call
 
 function weatherforcastapi(lat, lon) {
-    $.ajax({
-        url: "",
-        method: "GET",
-    }).then(function () {
-        // Log the queryURL
-        console.log(queryURL+APIKey);
+    // createCityList(citySearchList);
 
-        // Log the resulting object
-        console.log(weather);
+    // var queryURL2 =
+    // "https://api.openweathermap.org/data/2.5/forecast?&units=imperial&appid="+
+    // APIKey +
+    // "&q=" +
+    // city;
 
-        var nowMoment = moment();
+    // $.ajax({
+    //     url: "",
+    //     method: "GET",
+    // }).then(function (weather) {
+    //     // Log the queryURL
+    //     console.log(queryURL);
 
-        var displayMoment = $("<h3>");
-        $("#city-name").empty();
-        $("#city-name").append(
-            displayMoment.text("(" + nowMoment.format("M/D/YYYY") + ")")
-        );
+    //     // Log the resulting object
+    //     console.log(weather);
 
-        var cityName = $("<h3>").text(weather.name);
-        $("#city-name").prepend(cityName);
+    //     var nowMoment = moment();
 
-        var weatherIcon = $("<img>");
-        weatherIcon.attr(
-            "src",
-            "https://openweathermap.org/img/w/" + weather.weather[0].icon + ".png"
-        );
-        $("#current-icon").empty();
-        $("#current-icon").append(weatherIcon);
+    //     var displayMoment = $("<h3>");
+    //     $("#city-name").empty();
+    //     $("#city-name").append(
+    //         displayMoment.text("(" + nowMoment.format("M/D/YYYY") + ")")
+    //     );
 
-        $("#current-temp").text("Temperature: " + weather.main.temp + " °F");
-        $("#current-humidity").text("Humidity: " + weather.main.humidity + "%");
-        $("#current-wind").text("Wind Speed: " + weather.wind.speed + " MPH");
+    //     var cityName = $("<h3>").text(weather.name);
+    //     $("#city-name").prepend(cityName);
 
-        latitude = weather.coord.lat;
-        longitude = weather.coord.lon;
-    });
-}
+    //     var weatherIcon = $("<img>");
+    //     weatherIcon.attr(
+    //         "src",
+    //         "https://openweathermap.org/img/w/" + weather.weather[0].icon + ".png"
+    //     );
+    //     $("#current-icon").empty();
+    //     $("#current-icon").append(weatherIcon);
+
+    //     $("#current-temp").text("Temperature: " + weather.main.temp + " °F");
+    //     $("#current-humidity").text("Humidity: " + weather.main.humidity + "%");
+    //     $("#current-wind").text("Wind Speed: " + weather.wind.speed + " MPH");
+
+        var queryURL3 =
+        "https://api.openweathermap.org/data/2.5/uvi/forecast?&units=imperial&appid="+
+        APIKey+
+        "&q=&lat=" +
+        lat +
+        "&lon=" +
+        lon;
+
+      $.ajax({
+        url: queryURL3,
+        method: "GET"
+        // Store all of the retrieved data inside of an object called "uvIndex"
+      }).then(function(uvIndex) {
+        console.log(uvIndex);
+
+        var uvIndexDisplay = $("<button>");
+        uvIndexDisplay.addClass("btn btn-danger");
+
+        $("#current-uv").text("UV Index: ");
+        $("#current-uv").append(uvIndexDisplay.text(uvIndex[0].value));
+        console.log(uvIndex[0].value);
+
+        $.ajax({
+          url: queryURL2,
+          method: "GET"
+          // Store all of the retrieved data inside of an object called "forecast"
+        }).then(function(forecast) {
+          console.log(queryURL2);
+
+          console.log(forecast);
+          // Loop through the forecast list array and display a single forecast entry/time (5th entry of each day which is close to the highest temp/time of the day) from each of the 5 days
+          for (var i = 6; i < forecast.list.length; i += 8) {
+            // 6, 14, 22, 30, 38
+            var forecastDate = $("<h5>");
+
+            var forecastPosition = (i + 2) / 8;
+
+            console.log("#forecast-date" + forecastPosition);
+
+            $("#forecast-date" + forecastPosition).empty();
+            $("#forecast-date" + forecastPosition).append(
+              forecastDate.text(nowMoment.add(1, "days").format("M/D/YYYY"))
+            );
+
+            var forecastIcon = $("<img>");
+            forecastIcon.attr(
+              "src",
+              "https://openweathermap.org/img/w/" +
+                forecast.list[i].weather[0].icon +
+                ".png"
+            );
+
+            $("#forecast-icon" + forecastPosition).empty();
+            $("#forecast-icon" + forecastPosition).append(forecastIcon);
+
+            console.log(forecast.list[i].weather[0].icon);
+
+            $("#forecast-temp" + forecastPosition).text(
+              "Temp: " + forecast.list[i].main.temp + " °F"
+            );
+            $("#forecast-humidity" + forecastPosition).text(
+              "Humidity: " + forecast.list[i].main.humidity + "%"
+            );
+
+            $(".forecast").attr(
+              "style",
+              "background-color:dodgerblue; color:white"
+            
+              );
+            }
+          });
+        });
+    //   });
+  }
 
 function coutryflag(countrycode) {
-    // var countrycode = "FR";
-    $.ajax({
-        url: "https://restcountries.eu/rest/v2/alpha/" + countrycode,
-        method: "GET",
-    }).then(function (response) {
-        //  country flag code goes here
-        var imgOfFlag = $("<img>").attr("src", response.flag);
-
-        $(".flag").append(imgOfFlag);
-        $(".language").text("Language: " + response.languages[0].name);
-        $(".population").text("Population: " + response.population);
-        $(".currencies").text("Currencies: " + response.currencies[0].name);
-        $(".callingCode").text("Dialing code: +" + response.callingCodes);
-    });
+  // var countrycode = "FR";
+  $.ajax({
+    url: "https://restcountries.eu/rest/v2/alpha/" + countrycode,
+    method: "GET",
+  }).then(function (response) {
+    //  country flag code goes here
+    var imgOfFlag = $("<img>").attr("src", response.flag);
+    $(".flag").append(imgOfFlag);
+    $(".language").text("Language: " + response.languages[0].name);
+    $(".population").text(
+      "Population: " +
+        Number(parseFloat(response.population).toFixed(2)).toLocaleString("en")
+    );
+    $(".currencies").text("Currencies: " + response.currencies[0].name);
+    $(".callingCode").text("Dialing code: +" + response.callingCodes);
+  });
 }
